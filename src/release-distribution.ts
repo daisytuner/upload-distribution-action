886481288fbe3c2dda39385d9b2244f48c6301db
Ramon Wirsch <ramon.wirsch@gmail.com>
2026-02-13 17:52:12 +0100

    Add options to also upload metadata about the distro it was built for / on

--- src/release-distribution.ts
+++ src/release-distribution.ts
@@ -7,6 +7,14 @@ import axios from "axios";
 import fg from "fast-glob";
 import path from "path";
 
+export type DistroOsRelease = {
+    name?: string
+    id?: string
+    id_like?: string
+    platform_id?: string
+    version_id?: string
+}
+
 type GenerateDistributableUploadUrlPayload = {
     fileName: string
     sha256?: string
@@ -14,6 +22,7 @@ type GenerateDistributableUploadUrlPayload = {
     architecture?: string
     os?: string
     uploadId?: string
+    distro_meta?: DistroOsRelease
 }
 
 type DistributableUploadResponse = {
@@ -54,6 +63,9 @@ export async function uploadDistributable() {
     const architecture = process.env['INPUT_ARCHITECTURE']!;
     const os = process.env['INPUT_OS'] || undefined;
     const rest_url = process.env['INPUT_URL']!;
+    const distro_id = process.env['INPUT_DIST_ID'] || undefined;
+    const distro_version = process.env['INPUT_DIST_VERSION'] || undefined;
+    const distro_platform_id = process.env['INPUT_DIST_PLATFORM_ID'] || undefined;
 
     const matchedFiles = await fg(inputFile);
     if (matchedFiles.length === 0) {
@@ -83,7 +95,12 @@ export async function uploadDistributable() {
       version: version,
       architecture: architecture,
       sha256: sha256,
-      os: os
+      os: os,
+      distro_meta: {
+        id: distro_id,
+        version_id: distro_version,
+        platform_id: distro_platform_id
+      }
     }
     const response = await generateDistributableUploadUrl(token, reqPayload, rest_url)
 
